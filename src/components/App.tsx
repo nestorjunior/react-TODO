71ccb69467737f3a2dd7bf33a75846a110da0296
nestor.junior <nestor.junior@sabion.com.br>
2020-10-05 22:24:20 -0300

    include navbar component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Navbar from './Navbar';
 
 const App = () => {
     return (
         <div>
             <h1>My App</h1>
+            <Navbar></Navbar>
         </div>
     );
 }
