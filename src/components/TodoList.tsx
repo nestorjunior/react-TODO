10467afa3f1e35abf689325df43db7e50bb21a94
nestor.junior <nestor.junior@sabion.com.br>
2020-10-07 20:54:50 -0300

    init todo component items

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
+import { Todo } from '../models/Todo';
 
 const TodoList = () => {
+
+    const todos: Todo[] = [
+        { id: 1, title: 'Ir ao supermecado', done: false },
+        { id: 2, title: 'Passear com cachorro', done: true },
+    ];   
+
     return(
         <div>Todo Content</div>
     );
